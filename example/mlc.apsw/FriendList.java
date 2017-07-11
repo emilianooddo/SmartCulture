0a4c7d99da37527d0e433f02dc48bf43b2f48d24
Zatip <elpitaz93@gmail.com>
2017-07-12 00:22:44 +0200

    Add files via upload

--- example/mlc.apsw/FriendList.java
+++ example/mlc.apsw/FriendList.java
@@ -29,40 +29,42 @@ public class FriendList extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out=response.getWriter();  
-        request.getRequestDispatcher("friend_list.jsp").include(request, response);  
+		request.getRequestDispatcher("Friends.jsp").include(request, response);   
         response.setContentType("text/html;charset=UTF-8");
-       
+        String user1 = new String();
+        String user2 = new String();
         List<Utente> myFriends = new ArrayList<Utente>();
         String user;
         HttpSession session=request.getSession(false);  
-        if(session!=null){  
+        
          user=(String)session.getAttribute("id");
         
         try{
 
-  	 //loading drivers for mysql
+
            Class.forName("com.mysql.jdbc.Driver");
 
-   	 //creating connection with the database 
-           Connection con=DriverManager.getConnection
+ 
+           	Connection con=DriverManager.getConnection
                           ("jdbc:mysql://localhost:3306/esempio","root","01072014");
-           PreparedStatement ps =con.prepareStatement
-                               ("SELECT * FROM amicizie WHERE user_1 =? OR user_2=?");
-           ps.setString(1, user);
-           ps.setString(2, user) ;
-           ResultSet rs =ps.executeQuery();
            
-           
-           while(rs.next()) {
+	  				PreparedStatement ps =con.prepareStatement
+            ("SELECT anagrafica.*, amicizie.user_1 FROM anagrafica,amicizie WHERE ((amicizie.user_2 =? AND (amicizie.user_1 = anagrafica.id))) OR ((amicizie.user_1 =? AND (amicizie.user_2 = anagrafica.id)))");
+	  				
+	  		ps.setString(1, user);
+	  		ps.setString(2, user);
+	  		ResultSet rs3 =ps.executeQuery();
+           while(rs3.next()) {
         	   //request.getRequestDispatcher("index2.jsp").include(request, response);  
         	   Utente u = new Utente();
-               u.setNome(rs.getString("nome"));
-               u.setCognome(rs.getString("cognome"));
+               u.setNome(rs3.getString("nome"));
+               u.setCognome(rs3.getString("cognome"));
                myFriends.add(u);
               }
+          
            
            
-           for(Utente u: myFriends)
+           		for(Utente u: myFriends )
                out.println(u + "</br>");
            
   }catch(Exception e)
@@ -71,7 +73,7 @@ public class FriendList extends HttpServlet {
         }
                             
         out.close();  }
-	}
+	
 
 	
 
@@ -80,55 +82,9 @@ public class FriendList extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        List<Utente> myFriends = new ArrayList<Utente>();
-        String user;
-        HttpSession session=request.getSession(false);  
-        if(session!=null){  
-         user=(String)session.getAttribute("user");
         
-        try{
-
-  	 //loading drivers for mysql
-           Class.forName("com.mysql.jdbc.Driver");
-
-   	 //creating connection with the database 
-           Connection con=DriverManager.getConnection
-                          ("jdbc:mysql://localhost:3306/esempio","root","01072014");
-           PreparedStatement ps =con.prepareStatement
-                               ("SELECT * FROM amicizie WHERE user_1 =? OR user_2=?");
-           ps.setString(1, user);
-           ps.setString(2, user) ;
-           ResultSet rs =ps.executeQuery();
-           
-           
-           if(rs.next()) {
-        	   //request.getRequestDispatcher("index2.jsp").include(request, response);  
-        	   Utente u = new Utente();
-               u.setNome(rs.getString("nome"));
-               u.setCognome(rs.getString("cognome"));
-               myFriends.add(u);
-               
-            	
-            	
-       
-             
-            }else
-            {
-                out.println("Username or Password incorrect");
-                RequestDispatcher dsp = request.getRequestDispatcher("SimpleLogin.html");
-                
-                dsp.include(request, response);
-             }
-            
-            
-        }catch(Exception e)
-        {
-            e.printStackTrace();
-        }
                             
-        out.close();  }
+        
 	}
 
 }
